40f519d7a45e016e0d77bd958e6d2959d48da665
Bruno8060 <bbaque354@gmail.com>
2023-06-07 17:26:20 -0500

    Creación

---
+++ BaquePlua/Models/AplicationDBContext.cs
@@ -0,0 +1,23 @@
+﻿using BaquePlua.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BaquePlua.Models
+{
+    public class AplicationDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // El connectionString debe venir de un archivo de configuraciones
+            optionsBuilder.UseSqlServer("Server=DESKTOP-OI0V6HT\\SQLEXPRESS;Database=BaquePlua;Trusted_Connection=True")
+                .EnableSensitiveDataLogging(true)
+                .UseLoggerFactory(LoggerFactory.Create(builder =>
+                {
+                    builder.AddConsole().SetMinimumLevel(LogLevel.Information);
+                }));
+        }
+        public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<Genero> generos { get; set; }
+
+    }
+}
---
+++ BaquePlua/Models/Genero.cs
@@ -0,0 +1,10 @@
+﻿namespace BaquePlua.Models
+{
+    public class Genero
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; }  
+        public Boolean estado { get; set; }
+
+    }
+}
